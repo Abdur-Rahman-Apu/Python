bba722525532bd7765382b8aecfab0a385303734
Abdur-Rahman-Apu <60545278+Abdur-Rahman-Apu@users.noreply.github.com>
2021-01-27 17:31:00 -0800

    Variable in python

---
+++ Variable_in_python.py
@@ -0,0 +1,13 @@
+'''
+In python variable works as a tag.For values, memory location/id will be generated.
+
+Value is treated as object in python.
+'''
+
+print(id(10))
+a=10
+b=10
+print(f"The value of a is {a} and id is {id(a)}")
+print()
+print(f"The value of b is {b} and id is {id(b)}")
+
