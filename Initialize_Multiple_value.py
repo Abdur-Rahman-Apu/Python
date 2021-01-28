5509dbcfc7d9459be96547715e719575c5055135
Abdur-Rahman-Apu <60545278+Abdur-Rahman-Apu@users.noreply.github.com>
2021-01-27 17:31:57 -0800

    Initialize multiple value

---
+++ Initialize_Multiple_value.py
@@ -0,0 +1,12 @@
+#Initialize multiple variables value in one line in python
+
+a,b,c,d=1,2.0,'hello',[1,2,3]
+print(f"a={a}, b={b}, c={c}, d={d}")
+
+print()
+#Initialize same value in different variables
+a=b=c=d=1
+print(f"a={a}, b={b}, c={c}, d={d}")
+
+print()
+#
