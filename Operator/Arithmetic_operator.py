4492b76848314618a59e631dd160a2c1e3a28194
Abdur-Rahman-Apu <60545278+Abdur-Rahman-Apu@users.noreply.github.com>
2021-01-27 17:55:13 -0800

    Operator

---
+++ Operator/Arithmetic_operator.py
@@ -0,0 +1,15 @@
+
+a=1
+b=2
+#Additon(+)
+print(f"Addition of {a}+{b}={a+b}")
+#Subtraction(-)
+print(f"Subtraction of {a}-{b}={a-b}")
+#Multiplication(*)
+print(f"Multiplication of {a}*{b}={a*b}")
+#Division and floor division
+print(f"Division of {a}/{b}={a/b} and floor division={a//b}")
+#modulus operator
+print(f"Remainder of {a}%{b}={a%b}")
+#x to the power y -> x**y
+print(f"{a} to the power of {b} = {a**b}")
