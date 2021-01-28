f6e6a364cb5ef709766c27d3f7cb8e2e62eed134
Abdur-Rahman-Apu <60545278+Abdur-Rahman-Apu@users.noreply.github.com>
2021-01-27 17:41:22 -0800

    Escape sequence

---
+++ Escape_sequence_as_normal_text.py
@@ -0,0 +1,17 @@
+'''
+We can write as excape sequence as normal text in two ways.
+
+'''
+
+#Way no 1
+print("Hello\\nworld")
+print("Hello\\tworld")
+
+#way no 2
+print()
+'''
+RAW string
+'''
+
+print(r"Hello\nworld")
+print(r"Hello\tworld")
---
+++ Escape_sequence_in_python.py
@@ -0,0 +1,29 @@
+#newline
+# display like
+
+'''
+Hello
+world
+'''
+print("Hello \nworld")
+
+'''
+Tab -> means 4 spaces
+
+'''
+print("Hello\tworld")
+
+'''
+backslash 
+'''
+print("Hello\\world")
+
+''' 
+single quote under single qutoe
+'''
+print('Hello \'world\'')
+
+'''
+Double quote under double quote
+'''
+print("Hello\"world\"")
