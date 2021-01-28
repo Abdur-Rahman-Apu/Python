770cdb849d68e655594ddbb475cfd4b3e3cf3401
Abdur-Rahman-Apu <60545278+Abdur-Rahman-Apu@users.noreply.github.com>
2021-01-27 17:52:33 -0800

    One dimensional array

---
+++ Array/One-d-array.py
@@ -0,0 +1,13 @@
+'''
+Python support only one-d array
+import array
+arr_name=array.array('type code',[elements])
+
+'''
+
+from array import *
+a=array('i',[1,2,3,4])
+print(a)
+
+b=array('f',[1,2,3,4])
+print(b)
