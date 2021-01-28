988a061cab79b446040b7c83d36adfcba07fa5c7
Abdur-Rahman-Apu <60545278+Abdur-Rahman-Apu@users.noreply.github.com>
2021-01-27 17:48:51 -0800

    Basuc python

---
+++ Range function.py
@@ -0,0 +1,8 @@
+'''
+range(start,stop,step
+
+'''
+
+a=range(1,5,2)
+print(a[0])
+print(a[1])
---
+++ Type_conversion.py
@@ -0,0 +1,13 @@
+'''
+There are two types of type conversion:-
+1.Implicit
+2.Explicit: int(),float(),complex(),list(),tuple(),str(),set(),dict(),bin(),oct(),hex()
+
+'''
+#Implicit
+print(3/2)
+
+#Explicit
+a=1.0
+print(int(a))
+print(complex(a))
